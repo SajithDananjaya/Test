c8598ae353492d5bb7037e0c32a499f69a570035
SajithDananjaya <sajith.dt@hotmail.com>
2016-02-16 21:24:02 +0530

    Added Information

---
+++ testingSajith/src/testingSajith/MainClass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package testingSajith;
+
+/**
+ * @author Sajith Dananjaya
+ *
+ * <Sajith.dt@hotmail.com>
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Sajith Dananjaya");
+		// TODO Auto-generated method stub
+
+	}
+
+}

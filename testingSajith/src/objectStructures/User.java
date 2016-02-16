36e47b7a8bcd869bf5030f3cb1417d53a531f223
sajithdananjaya <sajith.dt@hotmail.com>
2016-02-16 22:50:52 +0530

    Costomized interface for common users

--- testingSajith/src/objectStructures/User.java
+++ testingSajith/src/objectStructures/User.java
@@ -3,11 +3,36 @@
  */
 package objectStructures;
 
+
+import java.util.HashMap;
 /**
  * @author Sajith Dananjaya
  *
  * <Sajith.dt@hotmail.com>
  */
-public class User {
+
+public abstract class User {
+	
+	private String userName;
+	private HashMap<String,Integer> musciTaste;
+	private int numberOfTags=0;
+	
+	
+	
+	
+	public final void setMusicTaste(String tagName,int tagCount){
+				
+	}
+	
+	public final int getTagCount(String tageName){
+		int tagCount=0;
+		return 0;
+	}
+	
+	public HashMap<String, Integer> getMusicTaste(){
+		return this.musciTaste;
+	}
+	
+	
 
 }
